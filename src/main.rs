dd9f517102b73a950a10f65b2a42da47b5811a62
Ytrog <hisbrucker@gmail.com>
2020-07-29 10:20:24 +0200

    it works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
+fn clean_str<'a>(s:&'a String) -> &'a str {
+    &s[..].trim()
+}
+
+fn ask(prompt:&str) -> u8 {
+    println!("{}", prompt);
+    let mut question = String::new();
+    io::stdin().read_line(&mut question).expect("leesfout");
+    u8::from_str_radix(clean_str(&question), 10).expect("Geen nummer")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rounds = ask("Hoeveel rondes?");
+    let players = ask("Hoeveel spelers?");
+    let time = ask("Hoeveel tijd in minuten?");
+    println!("Maximum speeltijd: {} minuten.", rounds * players * time);
 }
